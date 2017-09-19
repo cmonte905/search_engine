b13d24823136081182170ac8bc85931009cd69b4
j95alvarez <juanalvarez391@yahoo.com>
2017-09-19 11:37:41 -0700

    Added the json parser code and json files that were created

---
+++ json_parser.py
@@ -0,0 +1,16 @@
+import json
+import os.path
+# from pprint import pprint
+
+with open('all-nps-sites.json') as data_file:    
+    data = json.load(data_file)
+
+num = 1
+
+for document in data.get('documents'):
+	# Creates a new file in the corpous folder named json1.json
+	f = open(os.path.join("corpus/all-nps-sites", "json" + str(num) + ".json"), "w+")
+	# Grabs the body element from the json file and then writes to the json(num).json file
+	f.write(json.dumps(document))
+	num += 1
+	f.close()

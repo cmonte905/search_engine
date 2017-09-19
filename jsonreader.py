38898e89a00c4e3aa19480ec65c4899f8c94697f
cmonte905 <cmonte905>
2017-09-19 10:50:12 -0700

    Updated json reader(?) and removed __pycache__

--- jsonreader.py
+++ jsonreader.py
@@ -2,8 +2,14 @@ import json
 import os.path
 # from pprint import pprint
 
-def read_corpus(self):
+def read_corpus():
+    print 1
     with open('all-nps-sites.json') as data_file:    
         corpus_data = json.load(data_file)
+    print len(corpus_data['documents'])
     return corpus_data
 
+def read_mlb_corpus(self):
+    corpus_data = {}
+
+    print 1 

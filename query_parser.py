35c60cce813c65aa8a230f3f7571113abea2dcc4
cmonte905 <cmonte905>
2017-09-26 10:25:42 -0700

    Parser for query's work

--- query_parser.py
+++ query_parser.py
@@ -1,5 +1,5 @@
 from sys import exit
-import re
+from re import sub, findall, split
 
 
 def input_parser(user_string):
@@ -8,11 +8,10 @@ def input_parser(user_string):
     runs the appropiate commands
     """
     query_list = []
-    sub_list = list(re.findall(r'"([^"]*)"', user_string))
-    user_string = re.sub(r'"([^"]*)"', " ", user_string)
-    # print('User string after clean up:\n', user_string)
+    sub_list = list(findall(r'"([^"]*)"', user_string))
+    user_string = sub(r'"([^"]*)"', " ", user_string)
     query_list.append(sub_list)  # Added in the phrase literal
-    query_list.append(re.split(r'\s+', user_string.rstrip()))  # Removes spaces at the end of the string
+    query_list.append(split(r'\s+', user_string.rstrip()))  # Removes spaces at the end of the string
     print('Parsed out query list:\n', query_list)
 
 

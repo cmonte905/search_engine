3f39819a381e8b396f4195778fbfcce80113dc00
cmonte905 <cmonte905@hotmail.com>
2017-09-26 00:24:52 +0000

    Parsed out the users input

--- query_parser.py
+++ query_parser.py
@@ -4,29 +4,16 @@ import re
 
 def input_parser(user_string):
     """
-    First checks to see if there is a colon in the user's input and 
-    runs the appropiate commands 
+    First checks to see if there is a colon in the user's input and
+    runs the appropiate commands
     """
-
-    # if ':' in user_string:
-    #     print (user_string)
-    #     if ':q' in user_string:
-    #         exit()
-    #     if ':stem' in user_string:
-    #         print ("Will be stemming the token")
-    #     if ':index' in user_string:
-    #         print ('Will be indexing folder')
-    #     if ':vocab' in user_string:
-    #         print ('Will be spitting out words')
-
-    query_list = re.findall(r'"([^"]*)"', user_string)
+    query_list = []
+    sub_list = list(re.findall(r'"([^"]*)"', user_string))
     user_string = re.sub(r'"([^"]*)"', " ", user_string)
-    print(user_string)
-    # print("User string after getting rid of white space:\n", re.split(r'\s+', user_string))
-    # query_list.append(list(user_string.split(r'\s+')))
-    # query_list = list(filter(lambda x: x != '', query_list))
-    # print("User string, hopefully after getting the spaces removed:\n", user_string)
-    # print('Query list', query_list)
+    # print('User string after clean up:\n', user_string)
+    query_list.append(sub_list)  # Added in the phrase literal
+    query_list.append(re.split(r'\s+', user_string.rstrip()))  # Removes spaces at the end of the string
+    print('Parsed out query list:\n', query_list)
 
 
 def double_quotes(strings):

300b998ce85c2f844f9ad0026ca869b24b59d596
j95alvarez <juanalvarez391@yahoo.com>
2017-09-27 17:57:44 -0700

    wildcard and kgram fixes

--- KGramIndex.py
+++ KGramIndex.py
@@ -1,33 +1,11 @@
 class KGramIndex:
-	def __init__(self):
-		self.k_gram_index = []
-		self.vocab = {}
-
-
-	def add_string(self, word, k):
-		temp = ''
-		#new_k_gram = []
+	
+	def create_kgram(self, word, k):
+		kgram = []
 		for i in range(0, len(word)):
-			temp = word[i]
 			# starts at position i
-			if i + k <= len(word):
-				for j in range(i + 1, i + k):
-					temp += word[j]
-			else:
+			if i + k > len(word):
 				break
-			self.k_gram_index.append(temp)
-		return self.k_gram_index
-
-
-	def print_index(self):
-		s = ''
-		print (len(self.k_gram_index))
-		for i in range(0, len(self.k_gram_index)):
-			s += self.k_gram_index[i] + ' '
-		print (s)
-
-	def get_kgrams(self):
-		return self.k_gram_index
-
-	def reset(self):
-		self.k_gram_index = []
+				
+			kgram.append(word[i: i + k])
+		return kgram

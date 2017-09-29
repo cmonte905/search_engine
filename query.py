59c708a68a3de3191435eb76332de626437a868c
cmonte905 <cmonte905>
2017-09-29 15:05:16 -0700

    Made query into a class, added stuff to it

--- query.py
+++ query.py
@@ -2,19 +2,34 @@
 from re import sub, findall, split
 
 
-class QueryParser:
+class Query:
+    posting_lists = []
+    query_list = []
 
-    def input_parser(self, user_string):
+
+    def query_parser(self, user_string):
         """
         First checks to see if there is a colon in the user's input and
         runs the appropiate commands
         """
+        posting_lists = []
         query_list = []
+
         sub_list = list(findall(r'"([^"]*)"', user_string))
         user_string = sub(r'"([^"]*)"', " ", user_string)
         query_list.append(sub_list)  # Added in the phrase literal
-        query_list.append(split(r'\s+', user_string.rstrip()))  # Removes spaces at the end of the string
-        print('Parsed out query list:\n', query_list)
+        query_list.append(split(r'\s+', user_string.rstrip().lstrip()))  # Removes spaces at the end of the string
 
-    def double_quotes(self, strings):
+        if query_list[0]:  #
+            self.double_quotes_process(query_list[0])
+        if query_list[1]:
+            self.query_process(query_list[1])
+
+        return posting_lists
+
+    def query_process(self, string_list):
+        print(string_list)
+
+
+    def double_quotes_process(self, strings):
         print(strings)

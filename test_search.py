d3e5ba6606db79b9be1e6f9c3f3248a46a3e030c
Stanley Ung <stanleyung167@gmail.com>
2017-10-03 02:14:56 -0700

    test file

--- test_search.py
+++ test_search.py
@@ -54,28 +54,65 @@ def test_orquery():
 	or_list = q.or_list(l1, l2)
 	assert {1, 3, 4, 6, 10, 15, 50} == or_list
 
-def index_txt_file(file_name, documentID):
+def index_txt_file():
+    txt_index = positional_inverted_index()
     stemmer = Porter2Stemmer()
     k = kgram_index()
-    #punctuation = str.maketrans(dict.fromkeys(string.punctuation))
+
+    file_names = []
+    documentID = 1
 
     # Dealing with punctuation
     p = dict.fromkeys(string.punctuation)
     p.pop('-') # we need to deal with hyphens
     punctuation = str.maketrans(p)
 
-    try:
-        with open(file_name) as txt_file:
+    directory = path.dirname(path.realpath(__file__)) + '/unit_test_docs/'
+    chdir(directory)
 
-            content = txt_file.readlines();
-            content = content[0].lower().translate(punctuation).split(' ')
+    for file in listdir(directory):
+        if file.endswith('.txt'):
+            file_names.append(str(file))
 
-            content = list(filter(lambda w: w != '', map(lambda s: s.strip(), content)))
+    for file in file_names:
+        try:
+            with open(file) as txt_file:
 
-            term_positions = find_positions(content)
+                content = txt_file.readlines();
+                content = content[0].lower().translate(punctuation).split(' ')
+                content = list(filter(lambda w: w != '', map(lambda s: s.strip(), content)))
 
-            for key in term_positions:
-                index.add_term(stemmer.stem(key), documentID, term_positions[key])
-    except FileNotFoundError as e:
-        i = 0
-        print(e)
+                positions_dict = {}
+                for i in range(0, len(content)):
+                    if '-' in content[i]:
+
+                        hyphened_word_parts = content[i].split('-')
+                        hyphened_word = content[i].replace('-', '')
+                        hyphened_word_parts.append(hyphened_word)
+
+                        for word in hyphened_word_parts:
+                            if word in positions_dict:
+                                positions_dict[word].append(i)
+                            else:
+                                positions_dict[word] = [i]
+                    else:
+
+                        if content[i] in positions_dict:
+                            positions_dict[content[i]].append(i)
+                        else:
+                            positions_dict[content[i]] = [i]
+
+
+                for key in positions_dict:
+                    txt_index.add_term(stemmer.stem(key), documentID, positions_dict[key])
+        except FileNotFoundError as e:
+            i = 0
+            print(e)
+
+        documentID = documentID + 1
+
+    for key in txt_index.get_index():
+        txt_index.print_term_info(key)
+
+     
+

51917cf37ca61b60dbe957657d76ebfa1d7b1500
Stanley Ung <StanleyUng167@gmail.com>
2017-10-25 14:03:01 -0700

    converting hw files

--- disk_inverted_index.py
+++ disk_inverted_index.py
@@ -25,5 +25,54 @@ class disk_inverted_index:
 	        i = 0
 	        print(ex)
 
-
+	# Reads and returns a list of document IDs that contain the given term.
    	def get_postings(self, term):
+   		postings_position = self.binary_search_vocabulary(term)
+   		if postings_position >= 0:
+   			return read_posting_from_file(self.m_postings, postings_position)
+   		return 0
+
+   	# Locates the byte position of the postings for the given term.
+   	def binary_search_vocabulary(term):
+   		i = 0
+   		j = len(self.m_vocab_table) / 2 - 1
+
+   		while i <= j:
+   			try:
+   				m = (i + j) / 2
+   				v_list_position = self.m_vocab_table[m * 2]
+   				term_length = 0
+   				if m == len(self.m_vocab_table) / 2 - 1:
+   					term_length = int(len(self.m_vocab_list) - self.m_vocab_table[m * 2])
+   				else:
+   					term_length = int(m_vocab_table[(m + 1) * 2] - v_list_position)
+
+   			self.m_vocab_list.seek(v_list_position)
+
+   			mbuffer =  bytearray(term_length)
+   			self.m_vocab_list.read(mbuffer, 0, len(mbuffer))
+
+   			document_frequency = 0
+
+   			doc_ids = [document_frequency]
+
+   			except Exception as ex:
+   				print(ex)
+
+
+   	def read_vocab_table(index_name):
+   		try:
+   			table_file = open(index_name, 'r', 'vocabTable.bin')
+   			
+   			byte_buffer = bytearray(4)
+   			table_file.read(byte_buffer, 0, len(byte_buffer))
+
+   			table_index = 0
+
+   			vocab_table = 
+
+   		except Exception as e:
+   			print (ex)
+
+   	def get_term_count():
+   		return len(self.m_vocab_table) / 2

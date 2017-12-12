a45f9498014b2fdf87902646a2341beb8f793ca2
cmonte905 <cmonte905>
2017-12-11 22:27:26 -0800

    Rocchio seems kind of finsihed, it gives back some results, wrong, but results

--- m3_main.py
+++ m3_main.py
@@ -1,9 +1,7 @@
 from os import chdir, listdir, getcwd
 from math import log, sqrt, pow
-import json
 import string
 import re
-import unidecode
 import pprint
 
 # Custom Classes
@@ -18,11 +16,12 @@ import time
 # The Index
 # { index[term] : [<ID, [p1, p2,... pk]>, <ID, [p1, p2,... pk]>, ...] }
 all_docs_index = positional_inverted_index()
-hamilton_index = positional_inverted_index()
-jay_index = positional_inverted_index()
-madison_index = positional_inverted_index()
-disputed_index = positional_inverted_index()
+# hamilton_index = positional_inverted_index()
+# madison_index = positional_inverted_index()
 
+doc_wdt = {}
+ham_vector = {}
+mad_vector = {}
 # List of vocab tokens for terms in the corpus
 # Dictionary <String : Set<String>>
 
@@ -66,45 +65,60 @@ def index_file(file_name, documentID, index):
     for tf in weight_map:
         wdt += pow(1 + log(weight_map[tf]), 2)
     Ld = sqrt(wdt)
-    # pp = pprint.PrettyPrinter(indent=4)
-    # pp.pprint(weight_map)
-    # print('\n\n Fuck -', file_name)
+
+    for tf in weight_map:
+        weight_map[tf] = weight_map[tf]/Ld
+
+    doc_wdt[file_name] = weight_map
     # i_writer.write_ld(Ld)
 
 
-def index_dir(file_name, documentID, index):
-    stemmer = Porter2Stemmer()
-
-    # Dealing with punctuation
-    p = dict.fromkeys(string.punctuation)
-    p.pop('-')  # we need to deal with hyphens
-    punctuation = str.maketrans(p)
-    weight_map = {}
-
-    try:
-        with open(file_name) as txt_file:
-            body = txt_file.read().split(' ')
-            body = list(filter(lambda t: t != '' and t != '-', body))  # remove single spaces and single hyphens
-
-            position = 0
-            for term in body:
-                # take care of hyphenated words
-                if '-' in term:
-                    unhyphenated_word = term.replace('-', '')
-                    index.add_term(stemmer.stem(unhyphenated_word), documentID, position)
-                    hyphened_tokens = term.split('-')
-                    for t in hyphened_tokens:
-                        index.add_term(stemmer.stem(t), documentID, position)
+def train_rocchio(class_list, docs, name):
+    if name == 'hamilton':
+        vec = ham_vector
+    else:
+        vec = mad_vector
+    for i in docs:
+        if i in class_list:
+            inner_map = docs[i]
+            # print(name, ': ', i)
+            for j in inner_map:
+                if j not in vec:
+                    vec[j] = inner_map[j]
                 else:
-                    index.add_term(stemmer.stem(term), documentID, position)
-                position += 1
-                if term not in weight_map:
-                    weight_map[term] = 1
-                else:
-                    weight_map[term] = weight_map[term] + 1
+                    vec[j] = vec[j] + inner_map[j]
 
-    except FileNotFoundError as e:
-        print(e)
+    for i in vec:
+        vec[i] = vec[i]/len(class_list)
+
+
+def apply_rocchio(disputed_file):
+    scores = [0.0, 0.0]  # Hamilton and Madison respectively
+    disputed_index = doc_wdt[disputed_file]
+
+    sum_vec = 0.0
+    for i in disputed_index:
+        if i in ham_vector and (ham_vector[i] - disputed_index[i]) >= 0:
+            sum_vec += pow(ham_vector[i] - disputed_index[i], 2)
+    scores[0] = sqrt(sum_vec)
+
+    sum_vec = 0.0
+    for i in disputed_index:
+        if i in mad_vector and (mad_vector[i] - disputed_index[i]) >= 0:
+            sum_vec += pow(ham_vector[i] - disputed_index[i], 2)
+    scores[1] = sqrt(sum_vec)
+    print('File name:', disputed_file, scores)
+    pass
+
+
+def get_list_files(directory):
+    file_names = []  # Names of files
+    chdir(directory)
+    sorted_files = sorted(listdir(directory), key=lambda x: (int(re.sub('\D', '', x)), x))
+    for file in sorted_files:
+        if file.endswith('.txt'):
+            file_names.append(str(file))
+    return file_names
 
 
 def init(directory, index):
@@ -115,6 +129,7 @@ def init(directory, index):
     for file in sorted_files:
         if file.endswith('.txt'):
             file_names.append(str(file))
+
 
     # Index each file and mark its Document ID
     for file in file_names:
@@ -129,21 +144,24 @@ def main():
     # ------------------------------------------------------------------------------------------
     # Get rid of one things have been initialized
     init(all_files, all_docs_index)
-
     # i_writer = index_writer()
     # i_writer.write_index_to_disk(index.get_index())
     # ------------------------------------------------------------------------------------------
-    jay_files = '/Users/Cemo/Documents/cecs429/search_engine/federalist-papers/JAY'
-    init(jay_files, jay_index)
-    hamilton_files = '/Users/Cemo/Documents/cecs429/search_engine/federalist-papers/HAMILTON'
-    init(hamilton_files, hamilton_index)
-    madison_files = '/Users/Cemo/Documents/cecs429/search_engine/federalist-papers/MADISON'
-    init(madison_files, madison_index)
-    disputed_files = '/Users/Cemo/Documents/cecs429/search_engine/federalist-papers/DISPUTED'
-    init(disputed_files, disputed_index)
+    jay_files = get_list_files('/Users/Cemo/Documents/cecs429/search_engine/federalist-papers/JAY')
+    hamilton_files = get_list_files('/Users/Cemo/Documents/cecs429/search_engine/federalist-papers/HAMILTON')
+    madison_files = get_list_files('/Users/Cemo/Documents/cecs429/search_engine/federalist-papers/MADISON')
+
+    disputed_files = get_list_files('/Users/Cemo/Documents/cecs429/search_engine/federalist-papers/DISPUTED')
+
     chdir(cwd)  # come back to where the main is for the reading of files
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(jay_index.get_index())
+
+    train_rocchio(hamilton_files, doc_wdt, 'hamilton')
+    train_rocchio(madison_files, doc_wdt, 'madison')
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(ham_vector)
+    # pp.pprint(mad_vector)
+    for i in disputed_files:
+        apply_rocchio(i)
 
     corpus_size = len(listdir(all_files))
     print("Corpus size of all federalist papers are {0}".format(corpus_size))
